731d81d2400a4459e90e4088f13b7040a93ffec4
kihwankim <juljin1875@gmail.com>
2019-10-16 20:58:12 +0900

    fill done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,52 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// @small_character: custom name
+// @big_character: reversed name
+
+@color         // declare color variable
+M=0            // by default is white
+
+(loop)
+    @SCREEN    // RAM address 16384
+    D=A
+
+    @pixels 
+    M=D       // pixel address (starting point: 16384, max: 16384+8192=24576)
+
+    @KBD      // D = ascii code of a keyboard input
+    D=M
+
+    @black
+    D;JGT     // if(keyboard > 0) goto black
+
+    @color
+    M=0       // otherwise white
+    
+    @color_screen
+    0;JMP     // jump to subroutine that actually changes the color of screen
+
+    (black)
+        @color
+        M=-1  // set to black
+
+    (color_screen)
+        @color
+        D=M
+        @pixels
+        A=M
+        M=D   // M[pixels] = @color
+
+        @pixels
+        M=M+1
+        D=M
+
+        @24576 // loop until end of pixels
+        D=D-A
+        @color_screen
+        D;JLT
+
+@loop
+0;JMP         // infinite loop
+
+
